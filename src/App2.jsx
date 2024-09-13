73ef489eb14d995730f09f221d158f574a10a31a
sumit-brilworks <sumit.m@brilworks.com>
2024-09-13 16:13:14 +0530

    Added usememo code

---
+++ src/App2.jsx
@@ -0,0 +1,33 @@
+import React, { useMemo, useState } from "react";
+
+function App2() {
+  const [vat, setVal] = useState(0);
+  const callmemoFunc = useMemo(() => {
+    return new Promise((res) => setTimeout(() => res(vat), 5000));
+  }, [vat]);
+  const getMemo = () => {
+    console.log(callmemoFunc.then((res) => console.log(res)));
+  };
+  return (
+    <div>
+      <div>
+        <input
+          type="number"
+          onChange={(e) => {
+            setVal(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            getMemo();
+          }}
+        >
+          call memo
+        </button>
+        <div>{vat}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App2;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import App2 from "./App2.jsx";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <App2 />
+  </StrictMode>
+);
