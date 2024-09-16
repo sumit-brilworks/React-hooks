a4e048b2b030b7c3b5167ff698069cdd40dc5273
sumit-brilworks <sumit.m@brilworks.com>
2024-09-16 09:58:41 +0530

    Added Effect tags

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,19 +1,16 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import FormElement from "./components/FormElement";
-function App2() {
-  const ref = useRef();
-  console.log(ref.current);
-  // console.log(ref.current.getCount());
-  let getVar;
-  useEffect(() => {
-    ref.current.focus();
-  }, []);
+import { useEffect, useInsertionEffect, useLayoutEffect } from "react";
 
-  return (
-    <div>
-      <FormElement ref={ref} hello="hello" />
-    </div>
-  );
+function App2() {
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect is running ");
+  });
+  useEffect(() => {
+    console.log("UseEffect is running ");
+  });
+  useInsertionEffect(() => {
+    console.log("useInsertionEffect is running ");
+  });
+  return <div>Hello world</div>;
 }
 
 export default App2;
